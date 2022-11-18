a430e2f4c1a6665a517a3e877e7611df9e5df3e6
Peter Güttinger <njol@gmx.ch>
2022-11-18 16:49:13 +0100

    Fixed hard dependency on cloth config

--- src/main/java/ch/njol/minecraft/hudmod/options/ConfigMenu.java
+++ src/main/java/ch/njol/minecraft/hudmod/options/ConfigMenu.java
@@ -1,6 +1,7 @@
 package ch.njol.minecraft.hudmod.options;
 
-import ch.njol.minecraft.config.Config;
+import ch.njol.minecraft.config.ClothConfigSetup;
+import ch.njol.minecraft.config.ModMenuConfigSetup;
 import ch.njol.minecraft.hudmod.HudMod;
 import ch.njol.minecraft.uiframework.ElementPosition;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
@@ -16,14 +17,14 @@ public class ConfigMenu implements ModMenuApi {
 
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return Config.getModConfigScreenFactory(HudMod.MOD_IDENTIFIER + ".config", () -> HudMod.options, new Options());
+		return ModMenuConfigSetup.getModConfigScreenFactory(HudMod.MOD_IDENTIFIER + ".config", () -> HudMod.options, new Options());
 	}
 
 	public static void registerTypes() {
-		Config.registerType(ElementPosition.class, (value, defaultValue, field, translatePath, saveConsumer) -> {
+		ClothConfigSetup.registerType(ElementPosition.class, (value, defaultValue, field, translatePath, saveConsumer) -> {
 			List<AbstractConfigListEntry> entries = new ArrayList<>(); // API requires raw type
 			for (Field posField : ElementPosition.class.getDeclaredFields()) {
-				entries.add(Config.buildConfigEntry(value, defaultValue, posField, HudMod.MOD_IDENTIFIER + ".config.position"));
+				entries.add(ClothConfigSetup.buildConfigEntry(value, defaultValue, posField, HudMod.MOD_IDENTIFIER + ".config.position"));
 			}
 			return ConfigEntryBuilderImpl.create()
 				       .startSubCategory(new TranslatableText(translatePath), entries)
